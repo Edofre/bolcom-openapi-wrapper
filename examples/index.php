40f2b36c6ba50f3ab3b2796aac24d0885707fa05
Edo Freriks <edofre@gmail.com>
2018-07-27 17:05:18 +0200

    Update example code

--- examples/index.php
+++ examples/index.php
@@ -1,31 +1,56 @@
-
 <?php
 
+// Let's show some errors
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Make sure you installed the package
 require __DIR__ . '/vendor/autoload.php';
 
+// Create the client with your API key
 $client = new Edofre\BolCom\Client('YOURAPIKEYGOESHERE');
-$ping = $client->ping();
 
-var_dump($ping);
+/*
+|--------------------------------------------------------------------------
+| Ping
+|--------------------------------------------------------------------------
+*/
+try {
+    $ping = $client->ping();
+    var_dump($ping);
+} catch (Exception $e) {
+    var_dump($e);
+    exit;
+}
 
-// 1002004010708531 = inception
-$product = $client->product('1002004010708531', [
-    'type'              => 'raw',
-    'offers'            => 'all',
-    'includeattributes' => true,
-]);
+/*
+|--------------------------------------------------------------------------
+| Products
+| 1002004010708531 = inception
+| 123 does not exist
+|--------------------------------------------------------------------------
+*/
+try {
+    $product = $client->product('1002004010708531', [
+        'type'              => 'raw',
+        'offers'            => 'all',
+        'includeattributes' => true,
+    ]);
+    var_dump($product);
+} catch (Exception $e) {
+    var_dump($e);
+    exit;
+}
 
-var_dump($product);
-
-// 123 does not exist
-$unknownProduct = $client->product('123', [
-    'type'              => 'raw',
-    'offers'            => 'all',
-    'includeattributes' => true,
-]);
-
-var_dump($unknownProduct);
+try {
+    $unknownProduct = $client->product('123', [
+        'type'              => 'raw',
+        'offers'            => 'all',
+        'includeattributes' => true,
+    ]);
+    var_dump($unknownProduct);
+} catch (Exception $e) {
+    var_dump($e);
+    exit;
+}
